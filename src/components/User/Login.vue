4ba38cc508378f2df4f56091690f24b8d43f7773
CJR <524429470@qq.com>
2022-04-15 00:57:50 +0800

    feat(init): 增加配置文件. feat(User): 增加Login组件，引入element-ui

---
+++ src/components/User/Login.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="Login">
+      
+    <el-form
+      :label-position="labelPosition"
+      label-width="60px"
+      :model="formLabelAlign"
+    >
+      <el-form-item label="用户名">
+        <el-input v-model="formLabelAlign.username" placeholder="请输入用户名"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formLabelAlign.password" placeholder="请输入密码"></el-input>
+      </el-form-item>
+      <el-form-item label="验证码">
+        <el-input v-model="formLabelAlign.captcha" placeholder="请输入验证码"></el-input>
+      </el-form-item>
+      <el-button type="primary" @click="Login()">登录</el-button>
+    </el-form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: "Login",
+  data() {
+    return {
+      labelPosition: "right",
+      formLabelAlign: {
+        username: "",
+        password: "",
+        captcha: "",
+      },
+    };
+  },
+  methods:{
+      Login() {
+        //   axios.get()
+      }
+  }
+}
+</script>
+
+<style scoped>
+.Login {
+    /* margin-right: 50px; */
+    /* margin-left: 600px; */
+    margin-left: 60%;
+    margin-top: 10%;
+    width: 30%;
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import axios from 'axios'
 import App from './App.vue'
+
+Vue.use(ElementUI);
+Vue.prototype.$axios = axios
 
 Vue.config.productionTip = false
 
